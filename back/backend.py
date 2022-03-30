398754555548efcba27b493a2a8d3fc56a1f9019
ThiagoOAL <thiago-leite01@hotmail.com>
2022-03-29 23:43:57 -0300

    protótipo navegável

---
+++ back/backend.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("estrutura.html")
+
+@app.route('/sobre')
+def sobre():
+    return render_template("sobre.html")
+
+@app.route('/projetos')
+def projeto():
+    return render_template('projetos.html')
+
+@app.route('/paixao')
+def paixao():
+    return render_template('paixao.html')
+
+
+app.run(debug=True)
